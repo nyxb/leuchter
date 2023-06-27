70ca3a1021e5a1d12a05999a22cb120a50c4066e
nyxb <contact@nyxb.xyz>
2023-06-27 21:50:01 +0200

    ✨ feat(highlighter.ts): add support for syntax highlighting by adding functions to preload grammars, get grammar by alias, highlight tokens, highlight tokens with scopes, and highlight text.

---
+++ packages/leuchter/src/highlighter.ts
@@ -0,0 +1,122 @@
+import type { IGrammar } from 'vscode-textmate'
+import { Registry } from 'vscode-textmate'
+import {
+   createOnigScanner,
+   createOnigString,
+   loadWASM,
+} from 'vscode-oniguruma'
+
+// @ts-expect-error is fine
+import onig from 'vscode-oniguruma/release/onig.wasm'
+import { aliasToLangData } from './language'
+import { loadGrammarByScope } from './grammars'
+import type { LanguageAlias } from './language-data'
+import { tokenize, tokenizeWithScopes } from './tokenizer'
+import type { FinalTheme } from './theme'
+import type { Token } from './annotations'
+
+let registry: Registry | null = null
+
+export function preloadGrammars(languages: LanguageAlias[]) {
+   // initialize the registry the first time
+   if (!registry) {
+      const onigLibPromise = loadWASM(onig).then(() => ({
+         createOnigScanner,
+         createOnigString,
+      }))
+      registry = new Registry({
+         onigLib: onigLibPromise,
+         loadGrammar: scopeName => loadGrammarByScope(scopeName),
+      })
+   }
+
+   const promises = languages
+      .filter(alias => alias !== 'text')
+      .map((alias) => {
+         const langData = aliasToLangData(alias)
+         if (!langData)
+            throw new UnknownLanguageError(alias)
+
+         return registry.loadGrammar(langData.scopeName)
+      })
+
+   return Promise.all(promises)
+}
+
+export function getGrammar(alias: LanguageAlias): {
+   langId: string
+   grammar: IGrammar | null
+} {
+   if (alias === 'text') {
+      return {
+         langId: 'text',
+         grammar: null,
+      }
+   }
+
+   const langData = aliasToLangData(alias)
+   if (!langData)
+      throw new UnknownLanguageError(alias)
+
+   const grammar = getGrammarFromRegistry(langData.scopeName)
+   if (!grammar) {
+      throw new Error(
+      `Syntax highlighting error: grammar for ${alias} not loaded`,
+      )
+   }
+
+   return {
+      langId: langData.id,
+      grammar,
+   }
+}
+
+function getGrammarFromRegistry(scopeName: string) {
+   const { _syncRegistry } = registry as any
+   return _syncRegistry?._grammars[scopeName] as IGrammar
+}
+
+export class UnknownLanguageError extends Error {
+   alias: string
+   constructor(alias: string) {
+      super(`Unknown language: ${alias}`)
+      this.alias = alias
+   }
+}
+
+export function highlightTokens(
+   code: string,
+   grammar: IGrammar,
+   theme: FinalTheme,
+) {
+   registry.setTheme(theme)
+   const colorMap = getColorMap(theme)
+   return tokenize(code, grammar, colorMap)
+}
+
+function getColorMap(theme: FinalTheme) {
+   const colorMap = registry.getColorMap()
+   if (!theme.colorNames)
+      return colorMap
+   return colorMap.map((c) => {
+      const key = Object.keys(theme.colorNames).find(
+         key => theme.colorNames[key].toUpperCase() === c.toUpperCase(),
+      )
+      return key || c
+   })
+}
+
+export function highlightTokensWithScopes(
+   code: string,
+   grammar: IGrammar,
+   theme: FinalTheme,
+) {
+   registry.setTheme(theme)
+   const colorMap = registry.getColorMap()
+   return tokenizeWithScopes(code, grammar, colorMap)
+}
+
+export function highlightText(code: string): Token[][] {
+   const lines = code.split(/\r?\n|\r/g)
+   return lines.map(line => [{ content: line, style: {} }])
+}
