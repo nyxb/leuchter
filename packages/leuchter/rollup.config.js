757bdebcbd35312c2e0530cd60692ad371e75681
nyxb <contact@nyxb.xyz>
2023-06-27 21:47:16 +0200

    ✨ feat(rollup.config.js): add rollup configuration file for leuchter package to build cjs, esm and d.ts files. Add plugins for nodeResolve, commonjs, replace, json, typescript and arraybuffer. Add support for browser esm build with fake file-system.

---
+++ packages/leuchter/rollup.config.js
@@ -0,0 +1,44 @@
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import json from '@rollup/plugin-json'
+import typescript from 'rollup-plugin-typescript2'
+import dts from 'rollup-plugin-dts'
+import { arraybuffer } from './load-buffer'
+
+const pkg = require('./package.json')
+
+const plugins = [
+   replace({ __LIGHTER_VERSION__: `${pkg.version}`, preventAssignment: false }),
+   json(),
+   nodeResolve(),
+   typescript({
+      tsconfigDefaults: { compilerOptions: { module: 'esnext' } },
+   }),
+   arraybuffer({ include: '**/*.wasm' }),
+   commonjs(),
+]
+
+export default [
+   {
+      input: 'src/index.ts',
+      output: [
+         { file: 'dist/index.cjs.js', format: 'cjs' },
+         { file: 'dist/index.esm.mjs', format: 'esm' },
+      ],
+      plugins,
+   },
+   {
+      input: 'src/index.ts',
+      output: [{ file: 'dist/browser.esm.mjs', format: 'esm' }],
+      plugins: [
+         replace({ 'file-system': 'file-system.fake', 'preventAssignment': false }),
+         ...plugins,
+      ],
+   },
+   {
+      input: './src/index.ts',
+      output: [{ file: 'dist/index.d.ts', format: 'es' }],
+      plugins: [dts()],
+   },
+]
