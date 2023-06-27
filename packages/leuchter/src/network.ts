e613be8e14c227796c5ee8547d360714207d3364
nyxb <contact@nyxb.xyz>
2023-06-27 21:48:41 +0200

    ✨ feat(network.ts): add fetchJSON function to fetch JSON data from lighter.codehike.org server. This function supports both browser fetch and node https module. The function is used to fetch grammar and theme JSON files from the server. The LIGHTER_VERSION constant is added to the file and will be replaced at build time with the version from package.json.

---
+++ packages/leuchter/src/network.ts
@@ -0,0 +1,39 @@
+// this will be replaced at build time with the version from package json
+const LIGHTER_VERSION = '__LIGHTER_VERSION__'
+
+// endpoints:
+// /grammars/${name}.json
+// /themes/${name}.json
+export async function fetchJSON(endpoint: string) {
+   if (typeof fetch === 'function') {
+      // console.log(`using fetch`, `https://lighter.codehike.org/${endpoint}.json`);
+      const r = await fetch(`https://lighter.codehike.org/${endpoint}.json`)
+      return await r.json()
+   }
+   // console.log(`using https`, `https://lighter.codehike.org/${endpoint}.json`);
+
+   const https = await import('node:https')
+   const options = {
+      host: 'lighter.codehike.org',
+      path: `/${endpoint}.json`,
+      method: 'GET',
+   }
+
+   return new Promise((resolve, reject) => {
+      const req = https.request(options, (res) => {
+         let data = ''
+         res.on('data', (chunk) => {
+            data += chunk
+         })
+         res.on('end', () => {
+            resolve(JSON.parse(data))
+         })
+      })
+
+      req.on('error', (error) => {
+         reject(error)
+      })
+
+      req.end()
+   })
+}
