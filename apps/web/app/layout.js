76e20212062368cd95d73f3fe914dad0b78628d9
nyxb <contact@nyxb.xyz>
2023-06-28 09:40:51 +0200

    📦 chore(web): add Head component to set page title, meta tags, and favicon 📦 chore(web): add RootLayout component to provide a basic HTML structure for the app

---
+++ apps/web/app/layout.js
@@ -0,0 +1,10 @@
+import 'leuchter/themes/github-from-css.css'
+
+export default function RootLayout({ children }) {
+   return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+   )
+}
