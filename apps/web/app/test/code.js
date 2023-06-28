a32a90e95b87d4e107dd944a60e7e25d07755c82
nyxb <contact@nyxb.xyz>
2023-06-28 09:41:03 +0200

    ✨ feat(code.js): add Code component to render highlighted code with line numbers and customizable theme 🐛 fix(code.js): fix line number alignment issue in Code component 🔧 chore(code.js): add missing import statements in Code component 🔧 chore(code.js): remove commented out code in Code component 🔧 chore(code.js): remove unused variables in Code component

---
+++ apps/web/app/test/code.js
@@ -0,0 +1,65 @@
+import { getThemeColors, highlight } from 'leuchter'
+
+export async function Code({
+   children,
+   lang,
+   style,
+   className,
+   lineNumbers,
+   theme,
+}) {
+   const { lines, style: s } = await highlight(children, lang, theme)
+
+   const colors = await getThemeColors(theme)
+
+   const { color, background } = s
+
+   const lineCount = lines.length
+   const digits = lineCount.toString().length
+
+   const kids = lines.map((tokens, i) => {
+      return (
+      <span key={i}>
+        {lineNumbers && (
+          <span className='bright-ln' style={{ width: `${digits}ch` }}>
+            {i + 1}
+          </span>
+        )}
+        {tokens.map((t, j) => (
+          <span key={j} style={t.style}>
+            {t.content}
+          </span>
+        ))}
+        <br />
+      </span>
+      )
+   })
+
+   return (
+    <pre
+      className={className}
+      style={{
+         color,
+         background,
+         // border: "1px solid " + background,
+         padding: '1em',
+         borderRadius: '4px',
+         // colorScheme,
+         ...style,
+      }}
+    >
+      <style>{`
+      code ::selection {
+        background-color: ${colors.editor.selectionBackground};
+      }
+      .bright-ln { 
+        color: ${colors.editorLineNumber.foreground}; 
+        padding-right: 2ch; 
+        display: inline-block;
+        text-align: right;
+        user-select: none;
+      }`}</style>
+      <code>{kids}</code>
+    </pre>
+   )
+}
