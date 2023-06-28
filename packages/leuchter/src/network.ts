95a9c97ef48118147b72601b16145ac40c671e74
nyxb <contact@nyxb.xyz>
2023-06-28 09:00:51 +0200

    🚀 chore(leuchter): update package.json scripts 🚀 chore(leuchter): add @vitest/coverage-v8 package 🐛 fix(network.ts): update fetch endpoint URLs to use 'leuchter.nyxb.xyz' instead of 'lighter.codehike.org' for better consistency and reliability

--- packages/leuchter/src/network.ts
+++ packages/leuchter/src/network.ts
@@ -6,15 +6,15 @@ const LIGHTER_VERSION = '__LIGHTER_VERSION__'
 // /themes/${name}.json
 export async function fetchJSON(endpoint: string) {
    if (typeof fetch === 'function') {
-      // console.log(`using fetch`, `https://lighter.codehike.org/${endpoint}.json`);
-      const r = await fetch(`https://lighter.codehike.org/${endpoint}.json`)
+      // console.log(`using fetch`, `https://leuchter.nyxb.xyz/${endpoint}.json`);
+      const r = await fetch(`https://leuchter.nyxb.xyz/${endpoint}.json`)
       return await r.json()
    }
-   // console.log(`using https`, `https://lighter.codehike.org/${endpoint}.json`);
+   // console.log(`using https`, `https://leuchter.nyxb.xyz/${endpoint}.json`);
 
    const https = await import('node:https')
    const options = {
-      host: 'lighter.codehike.org',
+      host: 'leuchter.nyxb.xyz',
       path: `/${endpoint}.json`,
       method: 'GET',
    }
