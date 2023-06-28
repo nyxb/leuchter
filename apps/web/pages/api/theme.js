db97c1b287e474c5fae65768c8e211488d1f8589
nyxb <contact@nyxb.xyz>
2023-06-28 09:41:49 +0200

    📦 chore(theme.js): add new API endpoint for fetching themes 🔒 chore(theme.js): add support for handling OPTIONS request method to enable CORS preflight requests 🔧 chore(theme.js): configure runtime as 'edge' for the theme API endpoint 🔧 chore(theme.js): set Cache-Control header to 's-maxage=1, stale-while-revalidate' for caching the response 🔧 chore(theme.js): set Access-Control-Allow-Methods header to 'GET' to allow GET requests to the API endpoint 🔧 chore(theme.js): set Access-Control-Allow-Origin header to '*' to allow requests from any origin 🐛 fix(theme.js): handle error cases when fetching the theme and return a 500 response with the error message

---
+++ apps/web/pages/api/theme.js
@@ -0,0 +1,42 @@
+import { NextResponse } from 'next/server'
+
+export const config = {
+   runtime: 'edge',
+}
+
+// api/theme?name=dark-plus&v=0.1.8
+export default async (req) => {
+   if (req.method === 'OPTIONS')
+      return new Response(null, { status: 200 })
+
+   const url = new URL(req.url)
+   const name = url.searchParams.get('name')
+   const version = url.searchParams.get('v')
+   console.log('fetching theme', name, version)
+
+   try {
+      const r = await fetch(
+      `https://unpkg.com/leuchter@${version}/themes/${name}.json`,
+      )
+
+      if (!r.ok) {
+         throw new Error(
+        `https://unpkg.com/leuchter@${version}/themes/${name}.json ${r.status} ${r.statusText}`,
+         )
+      }
+
+      const theme = await r.json()
+
+      const res = NextResponse.json(theme)
+
+      res.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate')
+      res.headers.set('Access-Control-Allow-Methods', 'GET')
+      res.headers.set('Access-Control-Allow-Origin', '*')
+
+      return res
+   }
+   catch (e) {
+      console.log('error fetching theme', e)
+      return new Response(e.message, { status: 500 })
+   }
+}
